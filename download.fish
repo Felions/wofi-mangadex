c811b26e73e95fd9b37a2baac813a3df76233732
Felions <macrel.zarzycki@proton.me>
2025-07-29 13:34:46 +0200

    v0.2.3 got contentRating working

--- download.fish
+++ download.fish
@@ -12,12 +12,8 @@ set baseURL (echo $querry | jq -r ".baseUrl")
 set hash (echo $querry | jq -r ".chapter.hash")
 set img (echo $querry | jq -r ".chapter.data[]")
 
-
-echo -e "\n\nhere\n\n"
-echo -e "$argv\n\n"
-
 mkdir -p "$HOME/.mangadex/$title/$chapter"
 
 for i in (seq (count $img))
-    wget -O "$HOME/.mangadex/$title/$chapter/$i" "$baseURL/$quality/$hash/$img[$i]"
+    wget -q -O "$HOME/.mangadex/$title/$chapter/$i" "$baseURL/$quality/$hash/$img[$i]"
 end
--- install.fish
+++ install.fish
@@ -4,15 +4,12 @@ git clone git@github.com:Felions/wofi-mangadex.git ~/wofi-mangadex
 
 chmod u+x ~/wofi-mangadex/wofi-mangadex
 
-rm -f ~/.local/bin/wofi-mangadex
-ln ~/wofi-mangadex/wofi-mangadex ~/.local/bin/
-
 mkdir -p ~/.mangadex/.data
 echo "set -g reader gwenview" > ~/.mangadex/.data/settings.fish
 echo "set -g lang 'en'" >> ~/.mangadex/.data/settings.fish
 echo "#quality data/data-saver -> high/low" >> ~/.mangadex/.data/settings.fish
 echo "set -g quality 'data'" >> ~/.mangadex/.data/settings.fish
 echo "#content ratings save, suggestive, erotica, pornoghaphic" >> ~/.mangadex/.data/settings.fish
-echo "set -g contentRating 'safe suggestive erotica pornographic'" >> ~/.mangadex/.data/settings.fish
+echo "set -g contentRating 'safe'" >> ~/.mangadex/.data/settings.fish
 echo "#wofi css file location" >> ~/.mangadex/.data/settings.fish
 echo "set -g config ~/.config/wofi/wofi_styles.css" >> ~/.mangadex/.data/settings.fish
